6bf079bf24bd3ac25ad0c2dd65839f55aa8a03d3
Kenny Paredes <jkparedes3@utpl.edu.ec>
2025-01-09 12:30:40 -0500

    Problema03

--- ejercicios/Problemas/src/problemas/Problema3.java
+++ ejercicios/Problemas/src/problemas/Problema3.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -15,7 +17,62 @@ public class Problema3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+
+        double nota1;
+        double nota2;
+        double nota3;
+        double nota4;
+        String mensaje;
+
+        System.out.println("ingrese la primera nota");
+        nota1 = entrada.nextDouble();
+
+        System.out.println("ingrese la segunda nota");
+        nota2 = entrada.nextDouble();
+
+        System.out.println("ingrese la tercera nota");
+        nota3 = entrada.nextDouble();
+
+        System.out.println("ingrese la cuarta nota");
+        nota4 = entrada.nextDouble();
+
+        mensaje = obtenerPromediosCualitativos(nota1, nota2, nota3, nota4);
+        mensaje = mayusculas(mensaje);
+
+        System.out.printf("%s", mensaje);
     }
-    
+
+    public static String obtenerPromediosCualitativos(double a, double b, 
+            double c, double d) {
+        double promedio;
+        String cualitativo = "";
+        String mensaje;
+        promedio = (a + b + c + d) / 4;
+        if (promedio <= 5) {
+            cualitativo = "Regular";
+        } else {
+            if (promedio > 5 && promedio <= 8) {
+                cualitativo = "Bueno";
+            } else {
+                if (promedio > 8 && promedio <= 9) {
+                    cualitativo = "Muy Bueno";
+                } else {
+                    if (promedio > 9 && promedio <= 10) {
+                        cualitativo = "Sobre saliente";
+
+                    }
+                }
+
+            }
+        }
+        mensaje = String.format("El promedio de las notas: %.2f, %.2f, %.2f, %"
+                + ".2f es %s", a, b, c, d, cualitativo);
+        return mensaje;
+    }
+
+    public static String mayusculas(String a) {
+        a = a.toUpperCase();
+        return a;
+    }
 }
--- ejercicios/Problemas/src/problemas/Problema5.java
+++ ejercicios/Problemas/src/problemas/Problema5.java
@@ -10,5 +10,36 @@ package problemas;
  * @author reroes
  */
 public class Problema5 {
+     int [][] arreglo1 = {{1,2,3}, {10,20,30 }, {100,6,2}};
+        int [][] arreglo2 = {{1,2,3}, {2,2,2}, {3,1,2}};
+        int [][] arreglo3 = new int[3][3];
+        
+        for (int i = 0; i < arreglo1.length; i++) {
+            for (int j = 0; j < arreglo1[i].length; j++) {
+                arreglo3[i][j] = obtenerMultiplicacion(arreglo1[i][j], 
+                        arreglo2[i][j]);
+            }
+        }
+        
+        obtenerReporte(arreglo1);
+        obtenerReporte(arreglo2);
+        obtenerReporte(arreglo3);
+    }
+    
+    public static void obtenerReporte(int[][] arreglo1){
+        String cadena = "";
+        for (int i = 0; i < arreglo1.length; i++) {
+            for (int j = 0; j < arreglo1[i].length; j++) {
+                cadena = String.format("%s%d\t", cadena, arreglo1[i][j]);
+            }
+            cadena = String.format("%s\n", cadena);
+        }
+        System.out.println(cadena);
+    }
+    
+    public static int obtenerMultiplicacion(int a, int b){
+        int operacion;
+        operacion = a * b;
+        return operacion;
     
 }
