b63ff61feb69d953153ba77618d4ea9373b1e1b6
Kenny Paredes <jkparedes3@utpl.edu.ec>
2025-01-09 12:21:21 -0500

    Problema02

--- ejercicios/Problemas/src/problemas/Problema2.java
+++ ejercicios/Problemas/src/problemas/Problema2.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -15,7 +17,66 @@ public class Problema2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        int num1 = 0;
+        System.out.println("Ingrese 1 para obtener el area de un cuadrado");
+        System.out.println("Ingrese 2 para obtener el area de un triangulo");
+        System.out.println("Ingrese 3 para obtener el area de un rectangulo");
+        num1 = entrada.nextInt();
+
+        if (num1 == 1) {
+            obtenerAreaCuadrado();
+        } else {
+            if (num1 == 2) {
+                obtenerAreaTriangulo();
+            } else {
+                if (num1 == 3) {
+                    obtenerAreaRectangulo();
+                }
+
+            }
+
+        }
+
     }
-    
+
+    public static void obtenerAreaCuadrado() {
+        Scanner entrada = new Scanner(System.in);
+        double lado = 0;
+        double area;
+        System.out.println("Ingrese la medida del lado del cuadrado");
+        lado = entrada.nextDouble();
+        area = lado * lado;
+        System.out.printf("El area del cuadrado de lado: %.2f , es %.2f", 
+                lado, area);
+    }
+
+    public static void obtenerAreaTriangulo() {
+        Scanner entrada = new Scanner(System.in);
+        double base = 0;
+        double altura = 0;
+        double area;
+        System.out.println("Ingrese la base del triangulo");
+        base = entrada.nextDouble();
+        System.out.println("Ingrese la altura del triangulo");
+        altura = entrada.nextDouble();
+        area = (base * altura) / 2;
+        System.out.printf("El area del triangulo de base: %.2f, de altura %.2f,"
+                + " es: %.2f", base, altura, area);
+    }
+
+    public static void obtenerAreaRectangulo() {
+        Scanner entrada = new Scanner(System.in);
+        double base = 0;
+        double altura = 0;
+        double area;
+        System.out.println("Ingrese la base del rectangulo");
+        base = entrada.nextDouble();
+        System.out.println("Ingrese la altura del rectangulo");
+        altura = entrada.nextDouble();
+        area = base * altura;
+        System.out.printf("El area del rectangulo de lado: %.2f y altura %.2f, "
+                + "es: %.2f", base, altura, area);
+
+    }
 }
